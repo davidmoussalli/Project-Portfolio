d6d93fab3c7b7cf4e1180568991c5c54395ae8ca
David Moussalli <35779765+dmoussalli@users.noreply.github.com>
2019-11-08 17:35:51 -0500

    Create HDTestData.java

---
+++ CS3310 - Data and File Structures/AVL Trees/HDTestData.java
@@ -0,0 +1,73 @@
+package pack;
+
+
+/** The HDTestData class is a class whose instance contains all the data from a single
+ * 		hard drive test.  - See References in class main.
+ * 
+ * @author David Moussalli
+ */
+public class HDTestData {
+	private String serialNum;
+	private String modelNum;
+	private String capacity;
+	private String hours;
+	
+	/** Constructor for the HDTestData class
+	 * 
+	 * @param serial
+	 * @param model
+	 * @param capacity
+	 * @param hours
+	 */
+	public HDTestData(String line) {
+		String[] lineAttributes = line.split(",");
+		
+		this.serialNum = lineAttributes[0];
+		this.modelNum = lineAttributes[1];
+		this.capacity = lineAttributes[2];
+		this.hours = lineAttributes[3];
+	}
+	
+//Public GET methods:
+	
+	/** Get method for the serial number.
+	 * 
+	 * @return returns the serial number.
+	 */
+	public String getSerialNum() {
+		return serialNum;
+	}
+	
+	/** Get method for the model number.
+	 * 
+	 * @return returns the modelNum.
+	 */
+	public String getModelNum() {
+		return modelNum;
+	}
+	
+	/** Get method for the capacity.
+	 * 
+	 * @return returns the capacity.
+	 */
+	public String getCapacity() {
+		return capacity;
+	}
+	
+	/** Get method for the hours.
+	 * 
+	 * @return returns the hours.
+	 */
+	public String getHours() {
+		return hours;
+	}
+	
+	
+	
+	/** toString() method used for testing...
+	 * Returns all attributes of the HDTestData object in a String format.
+	 */
+	public String toString() {
+		return String.format(serialNum + "\t" + modelNum + "\t" + capacity + "\t" + hours); 
+	}
+}
